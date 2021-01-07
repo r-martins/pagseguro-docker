b0078cd4a6827896ef17566585c69563beda0caf
Ricardo Martins <ricardo@ricardomartins.net.br>
2021-01-08 09:52:34 +1100

    Corrigindo deploy script mageuni

--- src/deploy_mageuni.sh
+++ src/deploy_mageuni.sh
@@ -19,7 +19,7 @@ mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "UPDATE core_config_d
 #git submodule update --recursive
 bin/magento setup:upgrade
 bin/magento deploy:mode:set --skip-compilation production
-bin/magento module:disable Magento_AdminNotification Amazon_Core Klarna_Core Magento_NewRelicReporting Magento_GoogleAnalytics Magento_GoogleAdwords Magento_Fedex Amazon_Login Amazon_Payment Klarna_Ordermanagement Magento_SwaggerWebapi Magento_SwaggerWebapiAsync Magento_Swagger Vertex_Tax Magento_GoogleOptimizer Klarna_Kp Magento_TwoFactorAuth
+bin/magento module:disable Magento_AdminNotification Klarna_KpGraphQl Klarna_Onsitemessaging Amazon_Core Klarna_Core Vertex_AddressValidation Magento_NewRelicReporting Magento_GoogleAnalytics Magento_GoogleAdwords Magento_Fedex Amazon_Login Amazon_Payment Klarna_Ordermanagement Magento_SwaggerWebapi Magento_SwaggerWebapiAsync Magento_Swagger Vertex_Tax Magento_GoogleOptimizer Klarna_Kp Magento_TwoFactorAuth
 bin/magento config:set --lock-env web/unsecure/base_url https://pagseguro-exemplo-m2.ricardomartins.net.br/
 bin/magento config:set --lock-env web/secure/base_url https://pagseguro-exemplo-m2.ricardomartins.net.br/
 bin/magento config:set --lock-env web/cookie/cookie_domain pagseguro-exemplo-m2.ricardomartins.net.br
@@ -27,10 +27,10 @@ bin/magento config:set --lock-env customer/address/street_lines 4
 bin/magento config:set --lock-env system/smtp/disable 1
 bin/magento config:set --lock-env admin/security/password_is_forced 0
 redis-cli flushall
-bin/magento setup:di:compile
 bin/magento indexer:set-mode schedule
 bin/magento setup:upgrade
 bin/magento setup:static-content:deploy pt_BR en_US
 bin/magento maintenance:disable
 bin/magento cache:enable
+bin/magento setup:di:compile
 rm -f $deployLock
