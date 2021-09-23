f5fdf8842a342fdd6eb77f30661bd404b987ea56
Ricardo Martins <ricardo@ricardomartins.net.br>
2021-09-23 14:35:02 +1000

    mudancas no arquivo de deploy pra colocar as credenciais da loja e instalar o modulo via composer

--- src/deploy_mageuni.sh
+++ src/deploy_mageuni.sh
@@ -15,11 +15,14 @@ git checkout master
 git reset --hard origin/master
 mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db < pagseguro_exemplo_m2.sql
 mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "UPDATE core_config_data set value='localhost' where path = 'catalog/search/elasticsearch7_server_hostname';"
-/usr/local/bin/composer require ricardomartins/pagseguro --ignore-platform-reqs
+php -dmemory_limit=-1 /usr/local/bin/composer require ricardomartins/pagseguro --ignore-platform-reqs
 #git submodule update --recursive
 bin/magento setup:upgrade
 bin/magento deploy:mode:set --skip-compilation production
 bin/magento module:disable Magento_AdminNotification Klarna_KpGraphQl Klarna_Onsitemessaging Amazon_Core Klarna_Core Vertex_AddressValidation Magento_NewRelicReporting Magento_GoogleAnalytics Magento_GoogleAdwords Magento_Fedex Amazon_Login Amazon_Payment Klarna_Ordermanagement Magento_SwaggerWebapi Magento_SwaggerWebapiAsync Magento_Swagger Vertex_Tax Magento_GoogleOptimizer Klarna_Kp Magento_TwoFactorAuth Yotpo_Yotpo PayPal_Braintree
+bin/magento config:set payment/rm_pagseguro/merchant_email testepagseguro2@ricardomartins.net.br
+bin/magento config:set payment/rm_pagseguro/token d6e45405-cd1f-4a49-a9cd-ab696747a2459b60bdb1448ea31d4c487f2d9e3f85c1eb10-82f3-4302-bef5-9ca73ac0ea12
+bin/magento config:set payment/rm_pagseguro/key PUBD5921FCBD1454F7B9C8E6D1566566FF6
 bin/magento config:set --lock-env web/unsecure/base_url https://pagseguro-exemplo-m2.ricardomartins.net.br/
 bin/magento config:set --lock-env web/secure/base_url https://pagseguro-exemplo-m2.ricardomartins.net.br/
 bin/magento config:set --lock-env web/cookie/cookie_domain pagseguro-exemplo-m2.ricardomartins.net.br
