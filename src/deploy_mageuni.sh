38fe078299d27a6c37b64e94156774ed54dd24ad
Ricardo Martins <ricardo@ricardomartins.net.br>
2023-12-22 17:06:10 +1000

    Adicionando configs e instalacao do pagbank 4.x

--- src/deploy_mageuni.sh
+++ src/deploy_mageuni.sh
@@ -18,7 +18,7 @@ mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "UPDATE core_config_d
 mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO core_config_data (path, value) VALUES ('catalog/search/engine','elasticsearch7');"
 rm -rf vendor
 php -dmemory_limit=-1 ~/bin/composer.phar install --ignore-platform-reqs
-php -dmemory_limit=-1 ~/bin/composer.phar require ricardomartins/pagseguro --ignore-platform-reqs
+php -dmemory_limit=-1 ~/bin/composer.phar require ricardomartins/pagseguro ricardomartins/pagbank-magento2 --ignore-platform-reqs
 #git submodule update --recursive
 bin/magento setup:upgrade
 bin/magento deploy:mode:set --skip-compilation production
@@ -32,6 +32,29 @@ bin/magento config:set --lock-env web/cookie/cookie_domain pagseguro-exemplo-m2.
 bin/magento config:set --lock-env customer/address/street_lines 4
 bin/magento config:set --lock-env system/smtp/disable 1
 bin/magento config:set --lock-env admin/security/password_is_forced 0
+#pagbank 4.x
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank/public_key', 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr+ZqgD892U9/HXsa7XqBZUayPquAfh9xx4iwUbTSUAvTlmiXFQNTp0Bvt/5vK2FhMj39qSv1zi2OuBjvW38q1E374nzx6NNBL5JosV0+SDINTlCG0cmigHuBOyWzYmjgca+mtQu4WczCaApNaSuVqgb8u7Bd9GCOL4YJotvV5+81frlSwQXralhwRzGhj/A57CGPgGKiuPT+AOGmykIGEZsSD9RKkyoKIoc0OS8CPIzdBOtTQCIwrLn2FxI83Clcg55W8gkFSOS6rWNbG5qFZWMll6yl02HtunalHmUlRUL66YeGXdMDC2PuRcmZbGO5a/2tbVppW6mfSWG3NPRpgwIDAQAB');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank/connect_key', 'CONSANDBOX8F1E5FEC013685993973759AC5E79F');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_cc/active', '1');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_boleto/active', '1');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_pix/active', '1');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_cc/sort_order', '10');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_boleto/sort_order', '20');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_pix/sort_order', '30');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_cc_vault/active', '1');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_cc/title', 'Cartão de Crédito - via PagBank');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_cc/soft_descriptor', 'PagBankIntegracoes');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_cc/installments_options', 'external');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_cc/enable_installments_limit', '0');"
+
+bin/magento config:set payment/ricardomartins_pagbank/connect_key CONSANDBOX8F1E5FEC013685993973759AC5E79F
+bin/magento config:set payment/ricardomartins_pagbank_cc/active 1
+bin/magento config:set payment/ricardomartins_pagbank_boleto/active 1
+bin/magento config:set payment/ricardomartins_pagbank_pix/active 1
+bin/magento config:set payment/ricardomartins_pagbank_cc/sort_order 10
+bin/magento config:set payment/ricardomartins_pagbank_boleto/sort_order 20
+bin/magento config:set payment/ricardomartins_pagbank_pix/sort_order 30
+
 redis-cli flushall
 bin/magento indexer:set-mode schedule
 bin/magento setup:upgrade
