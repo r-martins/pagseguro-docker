f8c91167f10d1c642a76e1147781a796f8209d8e
Ricardo Martins <ricardo@ricardomartins.net.br>
2025-10-14 09:17:10 +1000

    Atualizando Connect key sandbox, pois a outra expirou

--- src/deploy_mageuni.sh
+++ src/deploy_mageuni.sh
@@ -35,7 +35,7 @@ bin/magento config:set --lock-env system/smtp/disable 1
 bin/magento config:set --lock-env admin/security/password_is_forced 0
 #pagbank 4.x
 mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, scope, scope_id, value) VALUES ('payment/ricardomartins_pagbank/public_key', 'default', 0, 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr+ZqgD892U9/HXsa7XqBZUayPquAfh9xx4iwUbTSUAvTlmiXFQNTp0Bvt/5vK2FhMj39qSv1zi2OuBjvW38q1E374nzx6NNBL5JosV0+SDINTlCG0cmigHuBOyWzYmjgca+mtQu4WczCaApNaSuVqgb8u7Bd9GCOL4YJotvV5+81frlSwQXralhwRzGhj/A57CGPgGKiuPT+AOGmykIGEZsSD9RKkyoKIoc0OS8CPIzdBOtTQCIwrLn2FxI83Clcg55W8gkFSOS6rWNbG5qFZWMll6yl02HtunalHmUlRUL66YeGXdMDC2PuRcmZbGO5a/2tbVppW6mfSWG3NPRpgwIDAQAB');"
-mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, scope, scope_id, value) VALUES ('payment/ricardomartins_pagbank/connect_key', 'default', 0, 'CONSANDBOX8F1E5FEC013685993973759AC5E79F');"
+mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, scope, scope_id, value) VALUES ('payment/ricardomartins_pagbank/connect_key', 'default', 0, 'CONSANDBOX795E98520284853531616BF851FF2B');"
 mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_cc/active', '1');"
 mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_boleto/active', '1');"
 mysql -u ricardo_user -p9erh74J5mfRvZrkZ -D magent1_db -e  "INSERT INTO magent1_db.core_config_data (path, value) VALUES ('payment/ricardomartins_pagbank_pix/active', '1');"
